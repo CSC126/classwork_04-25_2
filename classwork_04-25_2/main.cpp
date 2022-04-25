d71e1f3628cf42ea9c1f5f08126a93e4f5e01ed4
Maxim Voyevoda <maximvoyevoda@gmail.com>
2022-04-25 14:16:22 -0400

    Initial Commit

---
+++ classwork_04-25_2/main.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include<ctime>
+#include<cmath>
+using namespace std;
+
+//void curve(float &a);
+void randomGen();
+int addSum(int x, int y); //Prototype
+
+int x;
+
+int main() {
+
+//    float score;
+//    cout << "Enter your score: " << endl;
+//    cin >> score;
+//    curve(score);
+//    cout << "Score curved by 10 points: " << score << endl;
+
+    
+    
+//    for (int i = 0; i < 10; i++) {
+//        randomGen();
+//        cout << "Random Number: " << x << endl;
+//    }
+        
+    
+    int num1, num2;
+    
+    cout << "Enter 2 numbers: " << endl;
+    cin >> num1;
+    int sum = addSum(num1, num2);
+    cout << "Sum: " << sum << endl;
+    
+    
+    
+    return 0;
+}
+
+//void curve(float &a) {
+//    a+= 10;
+//}
+
+void randomGen() {
+    srand(time(0));
+    x = rand() % 30;
+}
+
+int addSum(int x, int y) {
+    return y*x;
+    
+    
+}
